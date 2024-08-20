52dc1a6f4baae3e4f8052b81d11cbfc654329929
Davide Sancin <98841916+dsancin@users.noreply.github.com>
2024-08-20 12:33:13 +0200

    Add files via upload

--- plots.R
+++ plots.R
@@ -2,8 +2,6 @@ library(copula)
 library(gridExtra)
 library(cowplot)
 library(gridGraphics)
-#?par per il problema dei margini cambia parametri mai, mar, oma, (vedi figura 1.6 e conversazione con chatgpt)
-#ora assi vanno bene, ma dimensione immagini è ancora meh
 setwd('/home/davide/università/tesi magistrale/tesi magistrale/figure')
 
 #CAPITOLO 1  
@@ -12,14 +10,14 @@ d<-2
 ic<-indepCopula(dim = d)
 set.seed(2008)
 #grid.arrange(plot1, plot2, ..., ncol=3, nrow = 3)
-pdf('01_independence_copula.pdf', width = 10, height = 10) #imposta width heigth uguali
-grid.arrange(wireframe2(ic, FUN=pCopula, col.4=adjustcolor("black", alpha.f=0.25)), contourplot2(ic, FUN=pCopula),ncol=2, nrow = 1)
+pdf('01_independence_copula.pdf', width = 10, height = 6) 
+grid.arrange(wireframe2(ic, FUN=pCopula, col.4=adjustcolor("black", alpha.f=0.25), cex.axis=1.5, cex.lab=1.5, , cex=1.5), contourplot2(ic, FUN=pCopula, cex.axis=1.5, cex.lab=1.5, , cex=1.5),ncol=2, nrow = 1)
 dev.off()
 
 #figure 1.2 (c-volume of ind cop)
 n<-1000
 U<-rCopula(n, copula=ic)
-pdf('02_c_volume_ind.pdf', width = 10, height = 10) #imposta width heigth uguali
+pdf('02_c_volume_ind.pdf', width = 10, height = 10) 
 par(mfrow=c(1,1),  mar=c(5,4,4,2)+0.6)
 plot(U, xlab=quote(U[1]), ylab=quote(U[2]),
      cex.lab=1.5, cex.axis=1.5)
@@ -33,10 +31,10 @@ dev.off()
 d<-2
 set.seed(2008)
 U<-runif(100)
-pdf('03_W_M.pdf', width = 10, height = 5) #imposta width heigth uguali
+pdf('03_W_M.pdf', width = 15, height = 10) #imposta width heigth uguali
 par(mfrow=c(1,2), mar=c(5,4,4,2)+0.6, pty="s")  #pty=s to make the plots square shaped
-plot(cbind(U,1-U), xlab=quote(U[1]), ylab=quote(U[2]))
-plot(cbind(U,U), xlab=quote(U[1]), ylab=quote(U[2]))
+plot(cbind(U,1-U), xlab=quote(U[1]), ylab=quote(U[2]), cex.axis=1.5, cex.lab=1.5, cex=1.5)
+plot(cbind(U,U), xlab=quote(U[1]), ylab=quote(U[2]), cex.axis=1.5, cex.lab=1.5, , cex=1.5)
 dev.off()
 
 #figure 1.4 (Survival copula)
@@ -59,33 +57,42 @@ incBeta <- function(x, a, b) pbeta(x, a, b) * beta(a, b) # incomplete beta
 psi <- function(t, th) t^(-1/th) * incBeta(pmin(1,t), a = 1/th, b = d) / th
 U <- psi(R * S, th = th)
 V<-1-U
-pdf('04_survival.pdf', width = 10, height = 5) #imposta width heigth uguali
-par(mfrow=c(1,2), pty='s')
-plot(U, xlab = quote(U[1]), ylab = quote(U[2])) # Pareto-simplex sample
-plot(V, xlab = quote(V[1]),  ylab = quote(V[2]))
+pdf('04_survival.pdf', width = 15, height = 10) #imposta width heigth uguali
+par(mfrow=c(1,2), mar=c(5,4,4,2)+0.6, pty="s")
+plot(U, xlab = quote(U[1]), ylab = quote(U[2]), cex.axis=1.5, cex.lab=1.5, , cex=1.5) # Pareto-simplex sample
+plot(V, xlab = quote(V[1]),  ylab = quote(V[2]), cex.axis=1.5, cex.lab=1.5, , cex=1.5)
 dev.off() 
 
 #figure 1.5 (radial and exchangeable copulas)
 #PER FARE GRID NON FUNZIONA SU PLOT NORMALI
 contourplot2(tCopula(0.5, df=2.5),
-             FUN=dCopula, n.grid=64, lwd=1/2)
+             FUN=dCopula, n.grid=64,     xlab = list(cex = 1.5),  # Modify x-axis label size
+             ylab = list(cex = 1.5),  # Modify y-axis label size
+             scales = list(
+               x = list(cex = 1.2),  # Modify x-axis tick label size
+               y = list(cex = 1.2)))   # Modify y-axis tick label size
+
 p2 <- grid.grab()
 contourplot2(gumbelCopula(3),
              FUN=dCopula, n.grid=64, lwd=1/4, 
              pretty=FALSE, cuts=42, 
-             col.regions=gray(seq(0.5,1, length.out=128)))
+             col.regions=gray(seq(0.5,1, length.out=128)),xlab = list(cex = 1.5),  # Modify x-axis label size
+             ylab = list(cex = 1.5),  # Modify y-axis label size
+             scales = list(
+               x = list(cex = 1.2),  # Modify x-axis tick label size
+               y = list(cex = 1.2)))   # Modify y-axis tick label size))
 p4 <- grid.grab()
-pdf('05_symmetry.pdf', width = 10, height = 5) #imposta width heigth uguali
-plot_grid(p2,p4)
+pdf('05_symmetry.pdf', width = 12, height = 6) #imposta width heigth uguali
+plot_grid(p2,p4,  rel_widths = c(1.2, 1.2), rel_heights = c(0.8, 0.8))
 dev.off()
 
-pdf('05_symmetry_obs.pdf', width = 10, height = 5) #imposta width heigth uguali
-par(mfrow=c(1,2), pty='s')
+pdf('05_symmetry_obs.pdf', width = 15, height = 10) #imposta width heigth uguali
+par(mfrow=c(1,2), mar=c(1,4,1,2)+0.6, pty="s")
 plot(rCopula(1000, copula=tCopula(0.5, df=2.5)),
-     xlab = quote(U[1]),  ylab = quote(U[2]))
+     xlab = quote(U[1]),  ylab = quote(U[2]), cex.axis=1.8, cex.lab=1.8, cex=1.5)
 
 plot(rCopula(1000, copula=gumbelCopula(3)),
-     xlab = quote(U[1]),  ylab = quote(U[2]))
+     xlab = quote(U[1]),  ylab = quote(U[2]), cex.axis=1.8, cex.lab=1.8,  cex=1.5)
 dev.off()
 
 
@@ -96,46 +103,46 @@ nc<- normalCopula(iTau(normalCopula(), tau = 0.5))
 U<-rCopula(n, copula=setTheta(nc, value=0.1))
 U2<-rCopula(n, copula = setTheta(nc, value=0.5))
 U3<-rCopula(n, copula=setTheta(nc, value=0.95))
-pdf('06_normal_copulas.pdf', width = 10, height = 5) #imposta width heigth uguali
-par(mfrow=c(1,3), mar=c(5,4,4,2)+0.6, pty='s')
+pdf('06_normal_copulas.pdf', width = 15, height = 6) #imposta width heigth uguali
+par(mfrow=c(1,3), mar=c(5,4,4,2)+1.1, pty='s')
 plot(U, xlab=quote(U[1]), ylab=quote(U[2]),
-     cex.lab=1.5, cex.axis=1.5)
+     , cex.axis=2.5, cex.lab=2.5, , cex=1.5)
 plot(U2, xlab=quote(U[1]), ylab=quote(U[2]),
-     cex.lab=1.5, cex.axis=1.5)
+     , cex.axis=2.5, cex.lab=2.5, , cex=1.5)
 plot(U3, xlab=quote(U[1]), ylab=quote(U[2]),
-     cex.lab=1.5, cex.axis=1.5)
+     , cex.axis=2.5, cex.lab=2.5, , cex=1.5)
 dev.off()
 
 
 #figure 1.7 (t copulas)
-pdf('07_t_copulas_fixed_df.pdf', width = 10, height = 5) #imposta width heigth uguali
-par(mfrow=c(1,3), mar=c(5,4,4,2)+0.6, pty='s')
+pdf('07_t_copulas_fixed_df.pdf', width = 15, height = 6) #imposta width heigth uguali
+par(mfrow=c(1,3), mar=c(1,4,1,2)+1.1, pty='s')
 plot(rCopula(1000, copula=tCopula(-0.99, df=2.5)),
      xlab = quote(U[1]),  ylab = quote(U[2]),
-     cex.lab=1.5, cex.axis=1.5)
+     cex.lab=2.5, cex.axis=2.5,cex=1.5)
 
 plot(rCopula(1000, copula=tCopula(0.5, df=2.5)),
      xlab = quote(U[1]),  ylab = quote(U[2]),
-     cex.lab=1.5, cex.axis=1.5)
+     cex.lab=2.5, cex.axis=2.5,cex=1.5)
 
 plot(rCopula(1000, copula=tCopula(0.99, df=2.5)),
      xlab = quote(U[1]),  ylab = quote(U[2]),
-     cex.lab=1.5, cex.axis=1.5)
+     cex.lab=2.5, cex.axis=2.5,cex=1.5)
 dev.off()
 
-pdf('07_t_copulas_fixed_P.pdf', width = 10, height = 5) #imposta width heigth uguali
-par(mfrow=c(1,3), mar=c(5,4,4,2)+0.6, pty='s')
+pdf('07_t_copulas_fixed_P.pdf', width = 15, height = 6) #imposta width heigth uguali
+par(mfrow=c(1,3), mar=c(1,4,1,2)+1.1, pty='s')
 plot(rCopula(1000, copula=tCopula(0.5, df=1)),
      xlab = quote(U[1]),  ylab = quote(U[2]),
-     cex.lab=1.5, cex.axis=1.5)
+     cex.lab=2.5, cex.axis=2.5,cex=1.5)
 
 plot(rCopula(1000, copula=tCopula(0.5, df=3)),
      xlab = quote(U[1]),  ylab = quote(U[2]),
-     cex.lab=1.5, cex.axis=1.5)
+     cex.lab=2.5, cex.axis=2.5,cex=1.5)
 
 plot(rCopula(1000, copula=tCopula(0.5, df=5)),
      xlab = quote(U[1]),  ylab = quote(U[2]),
-     cex.lab=1.5, cex.axis=1.5)
+     cex.lab=2.5, cex.axis=2.5,cex=1.5)
 dev.off()
 
 #figure 1.8 (archimedean copulas)
@@ -159,17 +166,17 @@ U.nf <- rCopula(n, copula = nf)
 U.gc <- rCopula(n, copula = gc)
 U.cc <- rCopula(n, copula = cc)
 
-pdf('08_archimedean_copulas.pdf', width = 10, height = 5) #imposta width heigth uguali
-par(mfrow=c(1,3), mar=c(5,4,4,2)+0.6, pty='s')
+pdf('08_archimedean_copulas.pdf', width = 15, height = 6) #imposta width heigth uguali
+par(mfrow=c(1,3), mar=c(5,4,4,2)+1.1, pty='s')
 plot(U.gc, xlab = expression(U[1]), ylab = expression(U[2]), # Gumbel copula
-     cex = 0.4,
-     cex.lab=1.5, cex.axis=1.5)
+     ,cex=1.5,
+     cex.lab=2.5, cex.axis=2.5)
 plot(U.cc, xlab = expression(U[1]), ylab = expression(U[2]), # Clayton copula
-     cex = 0.4,
-     cex.lab=1.5, cex.axis=1.5)
+     ,cex=1.5,
+     cex.lab=2.5, cex.axis=2.5)
 plot(U.nf, xlab = expression(U[1]), ylab = expression(U[2]), # Frank copula
-     cex = 0.4,
-     cex.lab=1.5, cex.axis=1.5)
+     ,cex=1.5,
+     cex.lab=2.5, cex.axis=2.5)
 dev.off()
 
 
@@ -180,37 +187,39 @@ library(copula)
 library(zoo)
 library(tseries) 
 load("/home/davide/università/tesi magistrale/dati/arpa/only_rain.RData")
-clean_rain<-as.matrix(clean_rain_xts)
-colnames(clean_rain)<-colnames(clean_rain_xts)
-clean_rain<-pobs(clean_rain)
+#clean_rain<-as.matrix(clean_rain_xts)
+#colnames(clean_rain)<-colnames(clean_rain_xts)
+#clean_rain<-pobs(clean_rain)
 
 
 #UNIVARITE TIME SERIES
-pdf('/home/davide/università/tesi magistrale/tesi magistrale/figure/cap3/rain_ts.pdf', width = 10, height = 10) #imposta width heigth uguali
+pdf('/home/davide/università/tesi magistrale/tesi magistrale/figure/cap3/rain_ts.pdf', width = 15, height = 14) #imposta width heigth uguali
 trace(plot.zoo, 
-      quote(mtext <- function(...) graphics::mtext(..., cex = 0.5, las = 1)))
-plot.zoo(clean_rain_xts, oma = c(6, 5, 5, 0), main=" ")
+      quote(mtext <- function(...) graphics::mtext(..., cex = 1.450, las = 3)))
+plot.zoo(clean_rain_xts, oma = c(7.5, 5, 5, 0), main=" ", cex.lab=1.8, cex.axis=1.8, nc=3)
 untrace(plot.zoo)
 dev.off()
 
+
 #ACF PLOTS
-pdf('/home/davide/università/tesi magistrale/tesi magistrale/figure/cap3/rain_acf1.pdf', width = 10, height = 10) #imposta width heigth uguali
+pdf('/home/davide/università/tesi magistrale/tesi magistrale/figure/cap3/rain_acf1.pdf', width = 8, height = 8) #imposta width heigth uguali
 par(mfrow=c(3,3))  
 for (x in colnames(clean_rain_xts[,1:9])){
-  acf(ts(clean_rain_xts[,x]), main=x)    #plot of the ACF
+  acf(ts(clean_rain_xts[,x]), main=x, cex.lab=1.5, cex.axis=1.5, cex.main=2.5)    #plot of the ACF
 }
 dev.off()
-pdf('/home/davide/università/tesi magistrale/tesi magistrale/figure/cap3/rain_acf2.pdf', width = 10, height = 10) #imposta width heigth uguali
+pdf('/home/davide/università/tesi magistrale/tesi magistrale/figure/cap3/rain_acf2.pdf', width = 8, height = 8) #imposta width heigth uguali
 par(mfrow=c(3,3))  
 for (x in colnames(clean_rain_xts[,10:18])){
-  acf(ts(clean_rain_xts[,x]), main=x)    #plot of the ACF
+  acf(ts(clean_rain_xts[,x]), main=x, cex.lab=1.5, cex.axis=1.5, cex.main=2.5)    #plot of the ACF
 }
 dev.off()
 
 #PAIRWISE SCATTERPLOT
-pdf('/home/davide/università/tesi magistrale/tesi magistrale/figure/cap3/pairwise_rain.pdf', width = 10, height = 10)
-pairs2(clean_rain, cex=0.4)
+pdf('/home/davide/università/tesi magistrale/tesi magistrale/figure/cap3/pairwise_rain.pdf', width = 11, height = 11)
+pairs2(clean_rain, cex=0.3, cex.labels=1.3, cex.axis=1.5)
 dev.off()
+
 
 #HEATMAPS
 library(heatmaply)
@@ -226,11 +235,11 @@ heatmaply_cor(upper_tdc, xlab = "Stations",
 dev.off()
 #K-PLOTS
 library(VineCopula)
-pdf('/home/davide/università/tesi magistrale/tesi magistrale/figure/cap3/Kplot.pdf', width = 10, height = 4)
-par(mfrow=c(1,3), pty='s')
-BiCopKPlot(clean_rain[,"Codroipo"] ,clean_rain[,"Tarvisio"])
-BiCopKPlot(clean_rain[,"Codroipo"] ,clean_rain[,"Musi"])
-BiCopKPlot(clean_rain[,"Codroipo"] ,clean_rain[,"Palazzolo.dello.Stella"])
+pdf('/home/davide/università/tesi magistrale/tesi magistrale/figure/cap3/Kplot.pdf', width = 12, height = 5)
+par(mfrow=c(1,3), mar=c(5,4,4,2)+1.1, pty='s')
+BiCopKPlot(clean_rain[,"Codroipo"] ,clean_rain[,"Tarvisio"], main="Codroipo \n Tarvisio", cex.lab=2, cex.axis=2, cex.main=2)
+BiCopKPlot(clean_rain[,"Codroipo"] ,clean_rain[,"Musi"], main="Codroipo \n Musi", cex.lab=2, cex.axis=2, cex.main=2)
+BiCopKPlot(clean_rain[,"Codroipo"] ,clean_rain[,"Palazzolo.dello.Stella"], main="Codroipo \n Palazzolo dello Stella", cex.lab=2, cex.axis=2, cex.main=2)
 dev.off()
 
 #eventually plot it only for the clusters, but due to reason above does not work anyway
@@ -242,8 +251,9 @@ dissimilarity_diff_upper<- ((1-upper_tdc))^0.5
 upper_clustering_diff_average<-hclust(d=as.dist(dissimilarity_diff_upper), method="average")
 upper_clustering_diff_complete <- hclust(d=as.dist(dissimilarity_diff_upper), method = "complete")
 
-pdf('/home/davide/università/tesi magistrale/tesi magistrale/figure/cap3/den.pdf', width = 10, height = 10) #imposta width heigth uguali
+pdf('/home/davide/università/tesi magistrale/tesi magistrale/figure/cap3/den.pdf', width = 18, height = 18) #imposta width heigth uguali
 par(mfrow=c(1,2))  
+par(cex = 1.8)  # Adjust 'cex' to control the label size
 plot(upper_clustering_diff_average, main="Average", xlab="", sub="")
 abline(h=0.91, col="blue")
 plot(upper_clustering_diff_complete, main="Complete", xlab="", sub="")
@@ -281,20 +291,20 @@ dev.off()
 pairwise14<-combn(colnames(cluster14), 2) #this create all the combinations of the stations (no repetitions)
 pdf('/home/davide/università/tesi magistrale/tesi magistrale/figure/cap3/Kplots_cluster14_1.pdf', width = 10, height = 10) #imposta width heigth uguali
 par(mfrow=c(3,3), pty='s')  #soluzione temporanea con più plot
-for (i in 1:(length(pairwise14)/4)){ 
-  BiCopKPlot(clean_rain[,pairwise14[1,i]],clean_rain[,pairwise14[2,i]], main = pairwise14[1:2,i])
+for (i in 1:(length(pairwise14)/4 -1)){ 
+  BiCopKPlot(clean_rain[,pairwise14[1,i]],clean_rain[,pairwise14[2,i]], main = pairwise14[1:2,i],cex.lab=2, cex.axis=2, cex.main=1.5)
 }
 dev.off()
 
 pdf('/home/davide/università/tesi magistrale/tesi magistrale/figure/cap3/Kplots_cluster14_2.pdf', width = 10, height = 10) #imposta width heigth uguali
 par(mfrow=c(3,3), pty='s')  #soluzione temporanea con più plot
-for (i in (length(pairwise14)/4):(length(pairwise14)/2)){ 
-  BiCopKPlot(clean_rain[,pairwise14[1,i]],clean_rain[,pairwise14[2,i]], main = pairwise14[1:2,i])
+for (i in (length(pairwise14)/4- 1):(length(pairwise14)/2)){ 
+  BiCopKPlot(clean_rain[,pairwise14[1,i]],clean_rain[,pairwise14[2,i]], main = pairwise14[1:2,i],cex.lab=2, cex.axis=2, cex.main=1.5)
 }
 dev.off()
 
 pdf('/home/davide/università/tesi magistrale/tesi magistrale/figure/cap3/scatter_cluster14.pdf', width = 10, height = 10) #imposta width heigth uguali
-pairs2(cluster14, main="")
+pairs2(cluster14, main="", cex=1.0, cex.labels=2.2, cex.axis=1.5)
 dev.off()
 
 
